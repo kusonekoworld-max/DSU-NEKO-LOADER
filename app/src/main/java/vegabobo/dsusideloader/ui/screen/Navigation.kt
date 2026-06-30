13664fdbc525a188359c5112c2ee458be0c8df47
kusonekoworld-max <kusonekoworld@gmail.com>
2026-06-30 11:02:20 +0700

    Update Navigation.kt
    Add GsiEditorScreen [skip ci]

--- app/src/main/java/vegabobo/dsusideloader/ui/screen/Navigation.kt
+++ app/src/main/java/vegabobo/dsusideloader/ui/screen/Navigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import vegabobo.dsusideloader.ui.gsiEditor.GsiEditorScreen
 import vegabobo.dsusideloader.ui.screen.about.AboutScreen
 import vegabobo.dsusideloader.ui.screen.adb.AdbScreen
 import vegabobo.dsusideloader.ui.screen.home.Home
@@ -16,6 +17,7 @@ object Destinations {
     const val ADBInstallation = "adb_installation"
     const val About = "about"
     const val Libraries = "libraries"
+    const val GsiEditor = "gsi_editor"
     const val Up = "up"
 }
 
@@ -36,5 +38,6 @@ fun Navigation() {
         composable(Destinations.ADBInstallation) { AdbScreen(navigate = { navigate(it) }) }
         composable(Destinations.About) { AboutScreen(navigate = { navigate(it) }) }
         composable(Destinations.Libraries) { LibrariesScreen(navigate = { navigate(it) }) }
+        composable(Destinations.GsiEditor) { GsiEditorScreen(navigate = { navigate(it) }) }
     }
 }
